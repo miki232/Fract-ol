faeb2e3d32692132e701b1c9ca93af534a9841ba
Michele Toia <mtoia@e4r5p2.42roma.it>
2022-05-23 15:48:44 +0200

    LOL

---
+++ init.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoia <mtoia@student.42roma.it>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 10:59:02 by mtoia             #+#    #+#             */
+/*   Updated: 2022/05/23 14:58:45 by mtoia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx/mlx.h"
+#include "so.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bit_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	ftclose(int keycode, t_vars *vars)
+{
+	if (keycode == ESC)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int	randomcolor()
+{
+	int x = rand();
+	while (x < rand())
+	{
+		return(rand() * 20 + 20 / 100);
+	}
+	return(rand() * 20);
+}
+int	mois(void)
+{
+	printf("GELL");
+	return (0);
+}
+int	main()
+{
+	t_vars	vars;
+	t_data	img;
+	int		y;
+	
+	y = 0;
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 640, 320, "Fract-ol");
+	img.img = mlx_new_image(vars.mlx, 640, 320);
+	img.addr = mlx_get_data_addr(img.img, &img.bit_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	while (y < 319)
+	{
+		my_mlx_pixel_put(&img, 5, y, 16711680);
+		y++;
+		int x;
+		x = 0;
+		while (x < 640)
+		{
+			my_mlx_pixel_put(&img, x, y, randomcolor());
+			x++;
+		}			
+	}
+	mlx_hook(vars.win, 2, 1L<<0, ftclose, &vars);
+	// mlx_hook(vars.win, 6, 1L<<4, mois, &vars); when mouse enter windows mask, mois fuction is enabled
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_loop(vars.mlx);
+}
